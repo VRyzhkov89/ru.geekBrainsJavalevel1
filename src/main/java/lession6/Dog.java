37ddb12a0e0266c865b43a4b34e97f2777555200
VRyzhkov89 <VRyzhkov89@mail.ru>
2020-11-22 22:28:14 +0300

    Initial commit

---
+++ src/main/java/lession6/Dog.java
@@ -0,0 +1,12 @@
+package lession6;
+
+
+public class Dog extends Animals {//наследующий класс
+    public Dog() {
+
+        this.type = "Пёс";
+        this.maxRun = 500;
+        this.maxSwim = 10;
+        this.maxJump = 0.5;
+    }
+}
